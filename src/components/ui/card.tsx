e3e4e0fb287ce0c26d08623ed7d36b26c9493561
lpercegona <lpercegona@gmail.com>
2026-02-13 12:38:09 -0300

    Update card.tsx

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,14 +9,14 @@ Card.displayName = "Card";
 
 const CardHeader = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
   ({ className, ...props }, ref) => (
-    <div ref={ref} className={cn("flex flex-col space-y-1.5 p-1", className)} {...props} />
+    <div ref={ref} className={cn("flex flex-col space-y-1.5 p-3", className)} {...props} />
   ),
 );
 CardHeader.displayName = "CardHeader";
 
 const CardTitle = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLHeadingElement>>(
   ({ className, ...props }, ref) => (
-    <h3 ref={ref} className={cn("text-2xl text-xs font-semibold leading-none tracking-tight", className)} {...props} />
+    <h3 ref={ref} className={cn("uppercase text-2xl text-xs font-semibold leading-none tracking-tight", className)} {...props} />
   ),
 );
 CardTitle.displayName = "CardTitle";
