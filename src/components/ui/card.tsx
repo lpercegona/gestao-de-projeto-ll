010ad3bf784ef9143492444467b5af3428af9ddc
lpercegona <lpercegona@gmail.com>
2026-02-13 12:39:53 -0300

    Update card.tsx

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -16,7 +16,7 @@ CardHeader.displayName = "CardHeader";
 
 const CardTitle = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLHeadingElement>>(
   ({ className, ...props }, ref) => (
-    <h3 ref={ref} className={cn("uppercase text-2xl !text-xs font-semibold leading-none tracking-tight", className)} {...props} />
+    <h3 ref={ref} className={cn(" text-2xl !text-xs font-semibold leading-none tracking-tight", className)} {...props} />
   ),
 );
 CardTitle.displayName = "CardTitle";
