95108458a3c49099b7addc7092c0855fbf43f59e
lpercegona <lpercegona@gmail.com>
2026-02-24 19:26:28 -0300

    Update Clients.tsx

--- src/pages/Clients.tsx
+++ src/pages/Clients.tsx
@@ -305,7 +305,7 @@ export const Clients: React.FC = () => {
                 return (
                   <Card
                     key={client.id}
-                    className="cursor-pointer hover:bg-accent/40 transition-bg"
+                    className="cursor-pointer "
                     onClick={() => navigate(`/clients/${client.id}`)}
                   >
                     <CardContent className="p-4">
