c7428b0fcf2e24996c59e91dbff2f0d9e1505794
lpercegona <lpercegona@gmail.com>
2026-02-25 11:10:19 -0300

    kanban

--- src/components/projects/ProjectKanbanView.tsx
+++ src/components/projects/ProjectKanbanView.tsx
@@ -383,7 +383,7 @@ export const ProjectKanbanView: React.FC<ProjectKanbanViewProps> = ({
                       <CardTitle className="text-sm font-medium flex items-center gap-2">
                         <Badge
                           variant="outline"
-                          className="text-[10px] font-bold text-background p-auto bg-[#000000]"
+                          className="text-[10px] border-transparent font-bold text-background px-1 bg-[#000000]"
                         >
                           {stageTasks.length}
                         </Badge>
